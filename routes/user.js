1de95b0315cecfa911008c9b2591bbc4b2de741e
iglesia.krisha1607 <iglesia.krisha1607@gmail.com>
2025-08-22 17:56:33 +0800

    added verify

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controller/user");
+const { verify } = require("../auth");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
